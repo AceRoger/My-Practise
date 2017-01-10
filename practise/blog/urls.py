b9e675031e784824c39e4565863a12167d6c6033
AceRoger <bhartis497@gmail.com>
2017-01-10 10:32:12 +0530

    directory removed

--- practise/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from blog import views
-
-
-urlpatterns = [
-    url(r'^$', views.test,name='test'),
-    url(r'^get-data/', views.get_data, name='get_data'),
-]
-
